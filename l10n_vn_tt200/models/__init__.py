0423591d04f198457649fa68e3e606bf38244782
Anh Vu <vuna2004@gmail.com>
2018-06-07 10:47:15 +0700

    [MIG] Migrate COA l10n_vn_tt200

---
+++ l10n_vn_tt200/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2009-2018 Trobz (http://trobz.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import account_account_tag
+from . import account_chart_template
+from . import account_journal
